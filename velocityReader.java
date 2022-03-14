f9aaade070daa854e98aa45dfcf498369ebd7b9f
Edoardo Piazza <e.piazza@student.maastrichtuniversity.nl>
2022-03-14 12:35:39 +0100

    change

--- velocityReader.java
+++ velocityReader.java
@@ -19,8 +19,7 @@ public class velocityReader{
     public static void main(String[] args) {
 
             //in the folllowing lines all the variables will be filled with the correspondant array values
-            getValues("/Users/edoardopiazza/Downloads/example_inputfile.txt");
-            
+            getValues("/Users/edoardopiazza/Desktop/trials.txt");
 
 
     }
@@ -43,7 +42,7 @@ public class velocityReader{
                 valueY = parts[0];
                 count++;
             }
-
+            velArray = new double[count][count];
             arrayX = xList.toArray(new String[0]); // converts from arraylist to array, it will return an array
             arrayY = yList.toArray(new String[0]); // converts from arraylist to array, it will return an array
             
@@ -56,6 +55,8 @@ public class velocityReader{
                 valueTemp=(Double.parseDouble(arrayY[i]));
                 velArray[1][i]=valueTemp;
             }
+
+            System.out.println(velArray[0][0]);
 
     
         }
