221524187bec78c6c226582df41134bdeed6a6bd
Gunes <gunes.ozmen02@gmail.com>
2022-03-14 11:58:55 +0100

    derivative

--- PhysicsEngine.java
+++ PhysicsEngine.java
@@ -7,9 +7,49 @@ public class PhysicsEngine{
     public double gravity;
     public double mass;
 
-    public int[] calculator(int positionX, int positionY, double[] velocity, double[][] frictionArray, double gravity){
-
+    public int[] calculator(double stepSize, int positionX, int positionY, double[] velocity, double[][] frictionArray, double gravity){
             
         return null;
+    }
+
+    public static double derivativeX(String pTerm, int val){
+        
+        String coeffString = "";
+        int i;
+        for(i = 0; pTerm.charAt(i) != 'x'; i++){
+            if(pTerm.charAt(i) == ' '){
+                continue;
+            }
+            coeffString += (pTerm.charAt(i));
+        }
+
+        long coeff = Long.parseLong(coeffString);
+
+
+        String powerString = "";
+
+        for(i = i + 2; i != pTerm.length() && pTerm.charAt(i) != ' '; i++){
+            
+            powerString += pTerm.charAt(i);
+        }
+
+        long power = Long.parseLong(powerString);
+
+        return coeff * power * (double)Math.pow(val, power-1);
+    }
+
+    static double derivativeVal(String poly, int val){
+
+        double ans = 0;
+
+        int i = 0;
+        String[] stSplit = poly.split("\\+");
+
+        while( i < stSplit.length){
+            ans = (ans + derivativeX(stSplit[i], val));
+            i++;
+        }
+
+        return ans;
     }
 }
