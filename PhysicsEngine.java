317ed16070d3ec34ce6ef8c1a85ca7abda6a3021
Gunes <gunes.ozmen02@gmail.com>
2022-03-15 10:51:13 +0100

    sads

--- PhysicsEngine.java
+++ PhysicsEngine.java
@@ -135,45 +135,4 @@ class PhysicsEngine{
             double second = mathFunction.Function(X+0.00000001,Y);
             return (second - first)/0.00000001;
         }
-    
-        public static double derivativeX(String pTerm, int val){
-            
-            String coeffString = "";
-            int i;
-            for(i = 0; pTerm.charAt(i) != 'x'; i++){
-                if(pTerm.charAt(i) == ' '){
-                    continue;
-                }
-                coeffString += (pTerm.charAt(i));
-            }
-    
-            long coeff = Long.parseLong(coeffString);
-    
-    
-            String powerString = "";
-    
-            for(i = i + 2; i != pTerm.length() && pTerm.charAt(i) != ' '; i++){
-                
-                powerString += pTerm.charAt(i);
-            }
-    
-            long power = Long.parseLong(powerString);
-    
-            return coeff * power * (double)Math.pow(val, power-1);
-        }
-    
-        static double derivativeVal(String poly, int val){
-    
-            double ans = 0;
-    
-            int i = 0;
-            String[] stSplit = poly.split("\\+");
-    
-            while( i < stSplit.length){
-                ans = (ans + derivativeX(stSplit[i], val));
-                i++;
-            }
-    
-            return ans;
-        }
     }
