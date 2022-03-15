5b721af7dba7857f943ca27f853a0229c1e41acf
Gunes <gunes.ozmen02@gmail.com>
2022-03-15 14:40:52 +0100

    derafds

--- TempTest.java
+++ TempTest.java
@@ -3,11 +3,12 @@ import org.mariuszgromada.math.mxparser.*;
 
 public class TempTest {
 
-    public static double derivativeOf(double X){
+    public static double derivativeOf(double X, double Y){
         
 
         Argument x1 = new Argument("x = "+X);
-        Expression e1 = new Expression("der((0.1*x)+1, x)", x1);
+        Argument y1 = new Argument("y = "+Y);
+        Expression e1 = new Expression("der((0.1*x)+(1+y), x)", x1, y1);
 
         double calc = e1.calculate();
 
@@ -16,6 +17,6 @@ public class TempTest {
     public static void main(String[] args) {
 
     
-        System.out.println(derivativeOf(2.4));
+        System.out.println(derivativeOf(2.4, 5.5));
     }
 }
